5381b942eebe9b67cfecab4e5e16c955278b1154
Patrick Kabuga <kabugap02@gmail.com>
2022-01-12 10:27:33 +0300

    Created a Head's Up Display

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +54,8 @@ getNewQuestion = () => {
         return window.location.assign('end.html');
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}  /  ${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -75,6 +79,10 @@ choices.forEach((choice) => {
 
         const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+        if(classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
@@ -84,6 +92,11 @@ choices.forEach((choice) => {
             
         });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
 
 startGame();
 
