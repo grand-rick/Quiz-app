dc7586b7bcf247c9225bee71a9dd4eb4fae87a26
Patrick Kabuga <kabugap02@gmail.com>
2022-01-12 12:24:29 +0300

    Saving HighScores in Local Storage

--- end.js
+++ end.js
@@ -4,10 +4,27 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener("keyup", () => {
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
-})
+});
 
 saveHighScore = (e) => {
     e.preventDefault();
-}
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
+};
