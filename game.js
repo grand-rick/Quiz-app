9c8d4ae928b27a1c280e573dbf92375e01d6c6cb
Patrick Kabuga <kabugap02@gmail.com>
2022-01-12 16:07:38 +0300

    Fetching API to Load Questions from Open Trivia DB API

--- game.js
+++ game.js
@@ -13,13 +13,28 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json')
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
     .then((res) => {
         return res.json();
     }).then((loadedQuestions) => {
-        questions = loadedQuestions;
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
         startGame();
-    }).catch((err) => {
+    })
+    .catch((err) => {
         console.error(err);
     });
 
