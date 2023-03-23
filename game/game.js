7024c88ac79f60d8224828a2deff1a03c3e070d0
Patrick Kabuga <kabugap02@gmail.com>
2023-03-23 18:07:29 +0300

    [fix]

--- game/game.js
+++ game/game.js
@@ -57,7 +57,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('end.html');
+        return window.location.assign('/end/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}  /  ${MAX_QUESTIONS}`;
